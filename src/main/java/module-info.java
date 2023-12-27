4f9d8655838b34400870c02485f383d4d0757ccd
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2023-12-28 02:31:11 +0600

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.rms {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.rms to javafx.fxml;
-    exports com.example.rms;
-}
