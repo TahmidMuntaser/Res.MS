9037a6dbda433701f07849b94eb9bd5205d267f9
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-01-20 22:55:33 +0600

    Update Configs.java

--- src/main/java/com/example/rms/Configs.java
+++ src/main/java/com/example/rms/Configs.java
@@ -3,8 +3,8 @@ package com.example.rms;
 public class Configs {
     protected static String dbHost = "bear-quetzal-7774.8nk.cockroachlabs.cloud";
     protected static String dbPort = "26257";
-    protected static String dbUser = "tahmid";
-    protected static String dbPassword = "jMuzBlhNDMQ5RFfnTVzTeA";
+    protected static String dbUser = "";
+    protected static String dbPassword = "";
     protected static String dbName = "cafe";
 
 }
